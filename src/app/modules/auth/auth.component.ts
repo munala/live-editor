734fb699158a3d63372fb4adc9bbbdf23d9bf86f
Oliver Munala <munalaoliverngoitsi@gmail.com>
2020-07-28 13:14:41 +0300

    change authService to public

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../core/services/auth/auth.service';
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
 }
